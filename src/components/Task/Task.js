5c4e257b63b90d2104df8f482a66fca5e09337c1
Dmitriy Panivnyk <d.panivnyk@ukr.net>
2022-10-31 18:01:11 +0200

    task end

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,17 +1,22 @@
-import { MdClose } from 'react-icons/md';
-import css from './Task.module.css';
+// Импортируем хук
+import { useDispatch } from 'react-redux';
+// Импортируем генератор экшена
+import { deleteTask } from '../../redux/actions';
 
 export const Task = ({ task }) => {
+  // Получаем ссылку на функцию отправки экшенов
+  const dispatch = useDispatch();
+
+  // Вызываем генератор экшена и передаём идентификатор задачи
+  // Отправляем результат - экшен удаления задачи
+  const handleDelete = () => dispatch(deleteTask(task.id));
+
   return (
-    <div className={css.wrapper}>
-      <input
-        type="checkbox"
-        className={css.checkbox}
-        checked={task.completed}
-      />
-      <p className={css.text}>{task.text}</p>
-      <button className={css.btn}>
-        <MdClose size={24} />
+    <div>
+      <input type="checkbox" />
+      <p>{task.text}</p>
+      <button type="button" onClick={handleDelete}>
+        Delete
       </button>
     </div>
   );
